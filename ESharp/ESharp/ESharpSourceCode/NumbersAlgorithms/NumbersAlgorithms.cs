244f239427c33918fa2440865bfd59852db91fed
Cigan <ciganoliviudavid@gmail.com>
2021-03-11 21:49:46 +0200

    Create LargestCommonDivisor Method

---
+++ ESharp/ESharp/ESharpSourceCode/NumbersAlgorithms/NumbersAlgorithms.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ESharp.ESharpSourceCode.NumbersAlgorithms
+{
+    public class NumbersAlgorithms : IAbstractNumbersAlgorithms
+    {
+        public int GetTheLargestCommonDivisor(int inferiorLimit, int superiorLimit)
+        {
+            if (superiorLimit == 0)
+                return 0;
+
+            var rest = inferiorLimit % superiorLimit;
+
+            while (rest != 0)
+            {
+                inferiorLimit = superiorLimit;
+                superiorLimit = rest;
+                rest = inferiorLimit % superiorLimit;
+            }
+
+            return superiorLimit;
+        }
+
+        public int GetTheLargestCommonDivisorRecursive(int inferiorLimit, int superiorLimit)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetTheLeastCommonMultiple(int inferiorLimit, int superiorLimit)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetPrimeValue(int factor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ReverseNumber(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetPalindromeNumber(int factor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float GetMeanOfTwoNumbers<T>(T firstNumber, T secondNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InterchangeVariablesValues<T>(ref T firstVariable, ref T secondVariable)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetDigitsSumOfNumber(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetDigitsProductOfNumber(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TestESharp/NumbersAlgorithmsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ESharp.ESharpSourceCode.NumbersAlgorithms;
+using NUnit.Framework;
+
+namespace TestESharp
+{
+    public class NumbersAlgorithmsTests
+    {
+
+        private IAbstractNumbersAlgorithms _numbersAlgorithms;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _numbersAlgorithms = NumbersAlgorithmsFactoryObject.GetNumbersAlgorithmsObject();
+        }
+
+        [Test]
+        public void Test_GetTheLargestCommonDivisor_()
+        {
+            Assert.IsTrue(_numbersAlgorithms.GetTheLargestCommonDivisor(8, 12) == 4);
+            Assert.IsTrue(_numbersAlgorithms.GetTheLargestCommonDivisor(1, 2) == 1);
+            Assert.IsTrue(_numbersAlgorithms.GetTheLargestCommonDivisor(36, 69) == 3);
+            Assert.IsTrue(_numbersAlgorithms.GetTheLargestCommonDivisor(100, 50) == 50);
+        }
+        
+    }
+}
