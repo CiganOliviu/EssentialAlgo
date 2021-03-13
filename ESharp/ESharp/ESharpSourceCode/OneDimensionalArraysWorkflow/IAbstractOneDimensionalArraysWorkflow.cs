74b9009a51e8cd5996a9bb560b6e1ba470f7f10f
Cigan <ciganoliviudavid@gmail.com>
2021-03-13 23:24:57 +0200

    Update IAbstractOneDimensionalArraysWorkflow

--- ESharp/ESharp/ESharpSourceCode/OneDimensionalArraysWorkflow/IAbstractOneDimensionalArraysWorkflow.cs
+++ ESharp/ESharp/ESharpSourceCode/OneDimensionalArraysWorkflow/IAbstractOneDimensionalArraysWorkflow.cs
@@ -2,6 +2,24 @@
 {
     public interface IAbstractOneDimensionalArraysWorkflow
     {
-        
+        T GetMinimumValueFromArray<T>(T[] array);
+        T GetMaximumValueFromArray<T>(T[] array);
+        T GetArrayElementsSum<T>(T[] array);
+        T GetArrayElementsProduct<T>(T[] array);
+        T GetArrayElementsDifference<T>(T[] array);
+        T GetArrayElementsDivision<T>(T[] array);
+        T GetArrayElementsMean<T>(T[] array);
+        bool IsArraySymmetric<T>(T[] array);
+        T AddValueInArray<T>(T[] array);
+        bool IsValueInArray<T>(T[] array, T value);
+        int[] ConvertNumberToArray(int number);
+        int ConvertArrayToNumber(int[] array);
+        T BoostUpArray<T>(T[] array, T booster);
+        T BoostDownArray<T>(T[] array, T booster);
+        T[] GetArraysSum<T>(T[] arrayOne, T[] arrayTwo);
+        T[] GetArraysProduct<T>(T[] arrayOne, T[] arrayTwo);
+        T[] GetArraysDifference<T>(T[] arrayOne, T[] arrayTwo);
+        T[] GetArraysDivision<T>(T[] arrayOne, T[] arrayTwo);
+        bool AreArraysEqual<T>(T[] arrayOne, T[] arrayTwo);
     }
 }
--- ESharp/ESharp/ESharpSourceCode/OneDimensionalArraysWorkflow/OneDimensionalArraysWorkflow.cs
+++ ESharp/ESharp/ESharpSourceCode/OneDimensionalArraysWorkflow/OneDimensionalArraysWorkflow.cs
@@ -2,6 +2,99 @@
 {
     public class OneDimensionalArraysWorkflow : IAbstractOneDimensionalArraysWorkflow
     {
-        
+        public T GetMinimumValueFromArray<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetMaximumValueFromArray<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetArrayElementsSum<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetArrayElementsProduct<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetArrayElementsDifference<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetArrayElementsDivision<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetArrayElementsMean<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsArraySymmetric<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T AddValueInArray<T>(T[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsValueInArray<T>(T[] array, T value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int[] ConvertNumberToArray(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ConvertArrayToNumber(int[] array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T BoostUpArray<T>(T[] array, T booster)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T BoostDownArray<T>(T[] array, T booster)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T[] GetArraysSum<T>(T[] arrayOne, T[] arrayTwo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T[] GetArraysProduct<T>(T[] arrayOne, T[] arrayTwo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T[] GetArraysDifference<T>(T[] arrayOne, T[] arrayTwo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T[] GetArraysDivision<T>(T[] arrayOne, T[] arrayTwo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AreArraysEqual<T>(T[] arrayOne, T[] arrayTwo)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
