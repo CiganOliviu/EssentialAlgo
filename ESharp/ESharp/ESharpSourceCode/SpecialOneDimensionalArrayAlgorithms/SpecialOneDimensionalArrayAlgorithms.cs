162b5705ae496edce57ded937a03090211ba181b
Cigan <ciganoliviudavid@gmail.com>
2021-03-18 20:44:47 +0200

    Implement BubbleSort Method

--- ESharp/ESharp/ESharpSourceCode/SpecialOneDimensionalArrayAlgorithms/SpecialOneDimensionalArrayAlgorithms.cs
+++ ESharp/ESharp/ESharpSourceCode/SpecialOneDimensionalArrayAlgorithms/SpecialOneDimensionalArrayAlgorithms.cs
@@ -4,9 +4,20 @@ namespace ESharp.ESharpSourceCode.SpecialOneDimensionalArrayAlgorithms
 {
     public class SpecialOneDimensionalArrayAlgorithms : IAbstractSpecialOneDimensionalArrayAlgorithms
     {
+        private static void SwapElements(ref int firstParameter, ref int secondParameter)
+        {
+            firstParameter += secondParameter;
+            secondParameter = firstParameter - secondParameter;
+            firstParameter -= secondParameter;
+        }
+        
         public void BubbleSort(IAbstractOneDimensionalArrayObject array)
         {
-            throw new System.NotImplementedException();
+            for (var it = 0; it < array.GetLengthOfOneDimensionalArray() - 1; it++)
+                for (var jit = 0; jit < array.GetLengthOfOneDimensionalArray() - it - 1; jit++)
+                    if (array.GetOneDimensionalArray()[jit] > array.GetOneDimensionalArray()[jit + 1])
+                        SwapElements(ref array.GetOneDimensionalArray()[jit],
+                            ref array.GetOneDimensionalArray()[jit + 1]);
         }
 
         public void MinimumValueSort(IAbstractOneDimensionalArrayObject array)
