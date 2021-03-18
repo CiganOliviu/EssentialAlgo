60db12d3360a2682fbd8e21507a197663f96dead
Cigan <ciganoliviudavid@gmail.com>
2021-03-18 20:50:06 +0200

    Implement SelectionSort Method

--- ESharp/ESharp/ESharpSourceCode/SpecialOneDimensionalArrayAlgorithms/SpecialOneDimensionalArrayAlgorithms.cs
+++ ESharp/ESharp/ESharpSourceCode/SpecialOneDimensionalArrayAlgorithms/SpecialOneDimensionalArrayAlgorithms.cs
@@ -36,7 +36,10 @@ namespace ESharp.ESharpSourceCode.SpecialOneDimensionalArrayAlgorithms
 
         public void SelectionSort(IAbstractOneDimensionalArrayObject array)
         {
-            throw new System.NotImplementedException();
+            for (var it = 0; it < array.GetLengthOfOneDimensionalArray(); it++)
+                for (var jit = it + 1; jit < array.GetLengthOfOneDimensionalArray(); jit++)
+                    if (array.GetOneDimensionalArray()[it] > array.GetOneDimensionalArray()[jit])
+                        SwapElements(ref array.GetOneDimensionalArray()[it], ref array.GetOneDimensionalArray()[jit]);
         }
 
         public void ShellSort(IAbstractOneDimensionalArrayObject array)
