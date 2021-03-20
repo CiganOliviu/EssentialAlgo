974e9db0c5e2c4676f2dda5df8f61899a30422ba
Cigan <ciganoliviudavid@gmail.com>
2021-03-20 19:41:31 +0200

    Implement GetMatricesProduct Method

--- ESharp/ESharp/ESharpSourceCode/MatricesWorkflow/MatricesWorkflow.cs
+++ ESharp/ESharp/ESharpSourceCode/MatricesWorkflow/MatricesWorkflow.cs
@@ -130,7 +130,16 @@ namespace ESharp.ESharpSourceCode.MatricesWorkflow
 
         public IAbstractMatrix GetMatricesProduct(IAbstractMatrix matrixOne, IAbstractMatrix matrixTwo)
         {
-            throw new System.NotImplementedException();
+            var result = MatrixFactoryObject.GetMatrixObject();
+            result.SetLineOfMatrix(matrixOne.GetLineOfMatrix());
+            result.SetColumnOfMatrix(matrixOne.GetColumnOfMatrix());
+            result.SetMatrix(matrixOne.GetMatrix());
+            
+            for (var it = 0; it < matrixOne.GetLineOfMatrix(); it++)
+                for (var jit = 0; jit < matrixOne.GetColumnOfMatrix(); jit++)
+                    result.GetMatrix()[it, jit] *= matrixTwo.GetMatrix()[it, jit];
+
+            return result;
         }
 
         private static int AddValueInArray(IAbstractMatrix matrix, int[] result, int index, int it, int jit)
