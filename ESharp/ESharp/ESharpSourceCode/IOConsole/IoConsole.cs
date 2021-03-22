0cec614ae79fa44cecbbfbd520b0f77fa4a7390e
Cigan <ciganoliviudavid@gmail.com>
2021-03-22 20:57:37 +0200

    Create Tree and IoConsole System

---
+++ ESharp/ESharp/ESharpSourceCode/IOConsole/IoConsole.cs
@@ -0,0 +1,28 @@
+﻿using ESharp.DataStructures.Matrix;
+using ESharp.DataStructures.OneDimensionalArray;
+
+namespace ESharp.ESharpSourceCode.IOConsole
+{
+    public class IoConsole : IAbstractIoConsole
+    {
+        public IAbstractOneDimensionalArrayObject ReadOneDimensionalArray(int size)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OutputOneDimensionalArray(IAbstractOneDimensionalArrayObject array)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAbstractMatrix ReadMatrix(int lines, int columns)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OutputMatrix(IAbstractMatrix matrix)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
